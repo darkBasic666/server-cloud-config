efa171286ad96d1c2a2b4c702cae3ed7b0abfad2
Maximiliano Paz <maximiliano.paz@wolox.com.ar>
2019-03-26 12:47:24 -0300

    initial commit

---
+++ src/main/java/wolox/cloudConfiguration/CloudConfigurationApplication.java
@@ -0,0 +1,15 @@
+package wolox.cloudConfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CloudConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigurationApplication.class, args);
+	}
+
+}
